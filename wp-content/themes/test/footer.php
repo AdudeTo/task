68060aaff4fbfc71a1dcece41fbbcb73dcc8f4a6
Vicho <vicho.vichev@ns-mediagroup.com>
2021-12-29 13:57:43 +0200

    dyn header

---
+++ wp-content/themes/test/footer.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * The template for displaying the footer
+ */
+
+?>
+
+
+<footer class="footer">FOOTER</footer>
+
+</body>
+</html>
---
+++ wp-content/themes/test/index.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * The main template file
+ */
+
+get_header();  ?>
+<main class="main">
+
+<h1 class="pageTitle">Lorem ipsum dolor sit amet</h1>
+<section class="leadPosts">
+    Lead Posts 123
+</section>
+<h2>secont title</h2>
+<section class="authorsList">
+    Authors
+</section>
+<section class="subscrybeForm">
+    The Form
+</section>
+    
+
+</main>
+
+
+<?php get_footer();
+?>
