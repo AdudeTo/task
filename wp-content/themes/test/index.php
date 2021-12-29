b721e5d22bf2f9cada41b5f0d3bfa1501be59121
Vicho <vicho.vichev@ns-mediagroup.com>
2021-12-29 14:28:12 +0200

    lead post setup

--- wp-content/themes/test/index.php
+++ wp-content/themes/test/index.php
@@ -8,7 +8,20 @@ get_header();  ?>
 
 <h1 class="pageTitle">Lorem ipsum dolor sit amet</h1>
 <section class="leadPosts">
-    Lead Posts 123
+    <ul>
+        <li class="--border">
+            <h3>post title</h3>
+            <span>exerp</span>
+        </li>
+        <li class="--border">
+            <h3>post title</h3>
+            <span>exerp</span>
+        </li>
+        <li class="--border">
+            <h3>post title</h3>
+            <span>exerp</span>
+        </li>
+    </ul>
 </section>
 <h2>secont title</h2>
 <section class="authorsList">
