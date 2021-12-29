b0b907786c5d596d54eb50b771678388899e97ac
Vicho <vicho.vichev@ns-mediagroup.com>
2021-12-29 11:05:26 +0200

    clean part of the code

--- wp-content/themes/twentytwentyone/archive.php
+++ wp-content/themes/twentytwentyone/archive.php
@@ -1,12 +1,6 @@
 <?php
 /**
- * The template for displaying archive pages
- *
- * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
- *
- * @package WordPress
- * @subpackage Twenty_Twenty_One
- * @since Twenty Twenty-One 1.0
+ * The template for displaying archive pages 
  */
 
 get_header();
