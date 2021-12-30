2f171660e97707b79fa0f60c5dbee3aa203210ab
Vicho <vicho.vichev@ns-mediagroup.com>
2021-12-30 16:26:45 +0200

    footer ajust texts and links

--- wp-content/themes/test/footer.php
+++ wp-content/themes/test/footer.php
@@ -6,7 +6,16 @@
 ?>
 
 
-<footer class="footer">FOOTER</footer>
+<footer class="footer">
+	<nav class="pageList">
+		<a href="#">Lorem ipsum</a>
+		<a href="#">Lorem ipsum</a>
+		<a href="#">Lorem ipsum</a>
+	</nav>
+	<span class="footerCaption">
+	Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc consequat quam sem, id facilisis odio tempus eu. Curabitur ut facilisis arcu, used vehicula risus. Ut faucibus ut sapien at dictum. Ut ut elementum sem. Proin vulputate purus mauris.
+	</span>
+</footer>
 
 </body>
 </html>
