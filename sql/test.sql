000af857f2513607893c7d513d0d243dba9ede4c
BuildTools <unconfigured@null.spigotmc.org>
2022-01-02 03:15:22 -0800

    prepare test link

---
+++ sql/test.sql
@@ -0,0 +1,16 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : localhost
+Source Server Version : 80023
+Source Host           : localhost:3306
+Source Database       : test
+
+Target Server Type    : MYSQL
+Target Server Version : 80023
+File Encoding         : 65001
+
+Date: 2022-01-02 03:02:55
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
